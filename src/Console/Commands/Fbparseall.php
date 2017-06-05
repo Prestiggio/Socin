cb89decbb0b46be77be4a928323e0a7782dbdc8e
Top Mora <manager@topmora.com>
2017-06-05 17:28:31 +0200

    facebook perse ability

--- src/Console/Commands/Fbparseall.php
+++ src/Console/Commands/Fbparseall.php
@@ -4,7 +4,8 @@ namespace Ry\Socin\Console\Commands;
 
 use Illuminate\Console\Command;
 use Facebook\Facebook;
-use Illuminate\Filesystem\Filesystem;
+use Facebook\GraphNodes\GraphNode;
+use Ry\Socin\Models\Facebooknode;
 
 class Fbparseall extends Command
 {
@@ -40,14 +41,27 @@ class Fbparseall extends Command
     public function handle()
     {
     	$fb = new Facebook([
-    			'app_id' => "691462271025098",
-    			"app_secret" => "635f60e1510231ea5bb5cae9a3f60b47",
-    			"default_graph_version" => "v2.9"
-    	]);
-    	
-    	$products = $fb->get("/1700007833570971/feed?fields=from,message,full_picture,picture,comments{comment_count},created_time&limit=5", env("ry_media_token"))->getGraphNode();
-    	
-    	$ar = $products->all();
+				'app_id' => "691462271025098",
+				"app_secret" => "635f60e1510231ea5bb5cae9a3f60b47",
+				"default_graph_version" => "v2.9"
+		]);
+		 
+		//$products = $fb->get("/1700007833570971/feed?fields=id,from,message,full_picture,picture,comments{comment_count,message,message_tags},attachments{subattachments},created_time&limit=5", env("ry_media_token"))->getGraphEdge();
+		$endpoint = $this->argument("endpoint");
+		$name = $this->argument("name");
+		$products = $fb->get($endpoint, env("ry_media_token"))->getGraphEdge();
+		$ar = $products->all();
+		$inserts = [];
+		foreach($ar as $a) {
+			/* @var $a GraphNode */
+			$inserts[] = [
+					"fbid" => $a->getField("id"),
+					"fbcreated" => $a->getField("created_time"),
+					"endpoint" => $endpoint,
+					"name" => $name
+			];
+		}
+		Facebooknode::insert($inserts);
     }
     
     public function getArguments() {
---
+++ src/Models/Facebooknode.php
@@ -0,0 +1,8 @@
+<?php
+namespace Ry\Socin\Models;
+
+use Illuminate\Database\Eloquent\Model;
+class Facebooknode extends Model
+{
+	protected $table = "ry_socin_facebooknodes";
+}
