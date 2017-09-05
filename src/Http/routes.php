b52a3e900cf8a8dda84dc54bb2abec5674786be1
folojona@gmail.com <topgasy@tsikawa.com>
2017-09-05 16:11:15 +0200

    Kipa - token longevizer

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,4 +1,11 @@
 <?php
+Route::get('ry/socin-token', function(\Illuminate\Http\Request $request){
+	return $request->get("short");
+	$ch = curl_init("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=691462271025098&client_secret=635f60e1510231ea5bb5cae9a3f60b47&fb_exchange_token=EAAJ04ZAsKP8oBANGbI3QsOpt83whsDq3DFXVTeLCCphlzM9v4eh1vvZCQvG4cpt5AITNhOirwlxGd3swagiUb40E7orjGaKKjnNZCZAKRWXZCynptvf1bX6MCb14afRbGVvIIWnLteS8Q6PwbgBQMunHjkLbE7ZAfMNtsoc8mYDWwg65JHGJkLzxp0u6maYrkZD");
+	curl_exec($ch);
+	curl_close($ch);
+});
+
 Route::group(["middleware" => "bot"], function(){
 	Route::get("ry/socin/forms", "JsonController@listForms");
 	Route::post("ry/socin/form/{form}/cancel", "JsonController@cancel");
